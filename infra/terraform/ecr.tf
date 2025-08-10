6ffd6ea212e573f6cbd958bb212c55bc26536dbc
Kota Yuhara <hotwater1367@gmail.com>
2025-08-10 16:49:08 +0900

    Fix formatting in ECR repository configuration

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "app" {
-  name = "earthdata-asset-resolver"
+  name   = "earthdata-asset-resolver"
+  region = aws.uswest2
   image_scanning_configuration {
     scan_on_push = true
   }
