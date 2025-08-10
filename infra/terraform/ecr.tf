cbba2dc44c678af47460d228215c56dffeba7db8
Kota Yuhara <hotwater1367@gmail.com>
2025-08-10 16:55:25 +0900

    Fix region declaration in ECR repository configuration

--- infra/terraform/ecr.tf
+++ infra/terraform/ecr.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "app" {
-  name   = "earthdata-asset-resolver"
-  region = aws.uswest2
+  name     = "earthdata-asset-resolver"
+  provider = aws.uswest2
   image_scanning_configuration {
     scan_on_push = true
   }
