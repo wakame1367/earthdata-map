e5ea92cf4fb3224e20506e9576f06198895c2597
Kota Yuhara <hotwater1367@gmail.com>
2025-08-10 22:22:10 +0900

    Refactor output variables in outputs.tf for clarity and consistency

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,6 +2,10 @@ output "resolver_lambda_role_arn" {
   value = aws_iam_role.resolver.arn
 }
 
-output "ecr_url" {
-  value = aws_ecr_repository.app.repository_url
+output "titiler_ecr_url" {
+  value = aws_ecr_repository.titiler.repository_url
+}
+
+output "earthdata_asset_resolver_ecr_url" {
+  value = aws_ecr_repository.earthdata_asset_resolver.repository_url
 }
